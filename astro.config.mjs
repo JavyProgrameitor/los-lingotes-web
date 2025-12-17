b6307f7e633fd73e10ebe5b74cdb0d7dc6af8376
JavyProgrameitor <85842248+JavyProgrameitor@users.noreply.github.com>
2025-12-17 10:29:20 +0100

    chore: switch to loslingotes.es domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://loslingotes.es",
   devToolbar: { enabled: false },
   vite: {
     plugins: [tailwindcss()],
