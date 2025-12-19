51c91a9137c481f43cf7c32aadc255c843e7d1b2
JavyProgrameitor <85842248+JavyProgrameitor@users.noreply.github.com>
2025-12-20 00:42:59 +0100

    Change new image and videos and add new social network tick-tock

---
+++ src/data/tictok.ts
@@ -0,0 +1,7 @@
+export const tictok = [
+  {
+    title: "Carnaval Talavera 2026",
+    image: "/tic-toc/tictok01.png",
+    url: "https://www.tiktok.com/@comparsaloslingotes/video/7554498714681855254",
+  },
+];
