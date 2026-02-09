656418acf928db1e210de477a11f356219913049
Javier <f.j.gg2@hotmail.com>
2026-02-09 21:11:09 +0100

    New images and figures public.

--- src/data/instagram.ts
+++ src/data/instagram.ts
@@ -1,6 +1,11 @@
 export const instagram = [
   {
     title: "Carnaval Talavera 2026",
+    image: "/instagram/instagram03.png",
+    url: "https://www.instagram.com/stories/comparsalingotes/3828818275379989375/",
+  },
+  {
+    title: "Carnaval Talavera 2026",
     image: "/instagram/instagram01.png",
     url: "https://www.instagram.com/p/DJOnyVSOaFt/",
   },
--- src/data/tictok.ts
+++ src/data/tictok.ts
@@ -1,5 +1,10 @@
 export const tictok = [
   {
+    title: "Ensayos Talavera 2026",
+    image: "/tic-toc/tictok03.png",
+    url: "https://tiktok.com/@comparsaloslingotes/video/7604659017851456790",
+  },
+  {
     title: "Día de preguntas",
     image: "/tic-toc/tictok02.png",
     url: "https://www.tiktok.com/@comparsaloslingotes/video/7594046508639571222",
